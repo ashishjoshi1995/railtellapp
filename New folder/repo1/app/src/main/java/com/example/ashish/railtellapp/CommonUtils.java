45dc6d7bd19ab756c44970563680100b9329f2c7
Ashish Joshi <ashish1995joshi@gmail.com>
2016-08-20 10:46:39 +0530

    sync commit

---
+++ New folder/repo1/app/src/main/java/com/example/ashish/railtellapp/CommonUtils.java
@@ -0,0 +1,8 @@
+package com.example.ashish.railtellapp;
+
+/**
+ * @author Ashish
+ * @since 8/20/2016
+ */
+public class CommonUtils {
+}
--- New folder/repo1/app/src/main/java/com/example/ashish/railtellapp/LoginContainer.java
+++ New folder/repo1/app/src/main/java/com/example/ashish/railtellapp/LoginContainer.java
@@ -1,7 +1,10 @@
 package com.example.ashish.railtellapp;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.example.ashish.railtellapp.Constants;
 
 public class LoginContainer extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class LoginContainer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_container);
+        int id = getIntent().getExtras().getInt("id");
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        switch (id){
+            case Constants.CHANGE_PASSWORD:
+               // fragmentTransaction.replace(R.id.content, Fragment.instantiate(this,
+                 //       EmployeeRegister.class.getName(),getIntent().getExtras()));
+                break;
+
+            case Constants.FORGOT_PASSWORD:
+                //fragmentTransaction.replace(R.id.content, Fragment.instantiate(this,
+                  //      CabRegistration.class.getName(),getIntent().getExtras()));
+                break;
+            case Constants.NEW_USER:
+                //fragmentTransaction.replace(R.id.content, Fragment.instantiate(this,
+                //      CabRegistration.class.getName(),getIntent().getExtras()));
+                break;
+
+        }
+        fragmentTransaction.commit();
+
     }
 }
