968736da5b901c1e51d645429454b187b7803811
Ashish Joshi <ashish1995joshi@gmail.com>
2016-08-23 12:09:31 +0530

    sync commit

--- New folder/repo1/app/src/main/java/com/example/ashish/railtellapp/LoginContainer.java
+++ New folder/repo1/app/src/main/java/com/example/ashish/railtellapp/LoginContainer.java
@@ -5,6 +5,9 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.ashish.railtellapp.Constants;
+import com.example.ashish.railtellapp.LoginSupportFragments.ChangePassword;
+import com.example.ashish.railtellapp.LoginSupportFragments.ForgotPassword;
+import com.example.ashish.railtellapp.LoginSupportFragments.NewUser;
 
 public class LoginContainer extends AppCompatActivity {
 
@@ -16,17 +19,17 @@ public class LoginContainer extends AppCompatActivity {
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         switch (id){
             case Constants.CHANGE_PASSWORD:
-               // fragmentTransaction.replace(R.id.content, Fragment.instantiate(this,
-                 //       EmployeeRegister.class.getName(),getIntent().getExtras()));
+                fragmentTransaction.replace(R.id.content, Fragment.instantiate(this,
+                        ChangePassword.class.getName(),getIntent().getExtras()));
                 break;
 
             case Constants.FORGOT_PASSWORD:
-                //fragmentTransaction.replace(R.id.content, Fragment.instantiate(this,
-                  //      CabRegistration.class.getName(),getIntent().getExtras()));
+                fragmentTransaction.replace(R.id.content, Fragment.instantiate(this,
+                        ForgotPassword.class.getName(),getIntent().getExtras()));
                 break;
             case Constants.NEW_USER:
-                //fragmentTransaction.replace(R.id.content, Fragment.instantiate(this,
-                //      CabRegistration.class.getName(),getIntent().getExtras()));
+                fragmentTransaction.replace(R.id.content, Fragment.instantiate(this,
+                      NewUser.class.getName(),getIntent().getExtras()));
                 break;
 
         }
