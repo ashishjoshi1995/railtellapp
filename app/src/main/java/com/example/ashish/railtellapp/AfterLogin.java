b2e652fb86d15947980a209ff588c744299b356a
gaurav <gauravwadhwani.gw@gmail.com>
2016-09-28 13:26:47 +0530

    form added

--- app/src/main/java/com/example/ashish/railtellapp/AfterLogin.java
+++ app/src/main/java/com/example/ashish/railtellapp/AfterLogin.java
@@ -9,8 +9,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -46,6 +49,66 @@ public class AfterLogin extends AppCompatActivity implements OnMapReadyCallback 
     ImageView view1,view2,view3;
     private GoogleMap mgoogleMap;
     static final LatLng TutorialsPoint = new LatLng(21 , 57);
+    Spinner year,date,state,district;
+    String[] y = {
+            "2016",
+            "2015",
+            "2014",
+            "2013",
+            "2012",
+            "2011"
+    };
+    String[] d = {
+            "001: 01/Jan - 08/Jan",
+            "009: 09/Jan - 16/Jan",
+            "017: 17/Jan - 24/Jan",
+            "025: 25/Jan - 01/Feb",
+            "033: 02/Feb - 09/Feb",
+            "041: 10/Feb - 17/Feb",
+            "049: 18/Feb - 25/Feb",
+            "057: 26/Feb - 05/Mar",
+            "065: 06/Mar - 13/Mar",
+            "073: 14/Mar - 21/Mar",
+            "081: 22/Mar - 29/Mar",
+            "089: 30/Mar - 06/Apr",
+            "097: 07/Apr - 14/Apr",
+            "105: 15/Apr - 22/Apr",
+            "113: 23/Apr - 30/Apr",
+            "121: 01/May - 08/May",
+            "129: 09/May - 16/May",
+            "137: 17/May - 24/May",
+            "145: 25/May - 01/Jun",
+            "153: 02/Jun - 07/Jun",
+            "161: 08/Jun - 17/Jun",
+            "169: 18/Jun - 25/Jun",
+            "177: 26/Jun - 03/Jul",
+            "185: 04/Jul - 11/Jul",
+            "193: 12/Jul - 19/Jul",
+            "201: 20/Jul - 27/Jul",
+            "209: 28/Jul - 04/Aug",
+            "217: 05/Aug - 12/Aug",
+            "225: 13/Aug - 20/Aug",
+            "233: 21/Aug - 28/Aug",
+            "241: 29/Aug - 05/Sep",
+            "249: 06/Sep - 14/Sep",
+            "257: 15/Sep - 21/Sep",
+            "265: 22/Sep - 29/Sep",
+            "273: 30/Sep - 07/Oct",
+            "281: 08/Oct - 15/Oct",
+            "289: 16/Oct - 23/Oct",
+            "297: 24/Oct - 31/Oct",
+            "305: 01/Nov - 08/Nov",
+            "313: 09/Nov - 16/Nov",
+            "321: 17/Nov - 24/Nov",
+            "329: 25/Nov - 02/Dec",
+            "337: 02/Dec - 10/Dec",
+            "345: 11/Dec - 18/Dec",
+            "353: 19/Dec - 26/Dec",
+            "361: 27/Dec - 31/Dec"
+    };
+    String[] s = {
+            "Punjab","Kerala","Tamil Nadu","Uttar Pradesh","Uttrakhand"
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,6 +118,75 @@ public class AfterLogin extends AppCompatActivity implements OnMapReadyCallback 
         view1 = (ImageView) findViewById(R.id.view1);
         view2 = (ImageView) findViewById(R.id.view2);
         view3 = (ImageView) findViewById(R.id.view3);
+        year =(Spinner)findViewById(R.id.year);
+
+        ArrayAdapter<String> adapter= new ArrayAdapter<String>(this,android.
+                R.layout.simple_spinner_dropdown_item ,y);
+
+        year.setAdapter(adapter);
+
+        year.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                // Get select item
+                int sid=year.getSelectedItemPosition();
+                Toast.makeText(getBaseContext(), "You have selected City : " + y[sid],
+                        Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // TODO Auto-generated method stub
+            }
+        });
+
+    date =(Spinner)findViewById(R.id.date);
+
+    ArrayAdapter<String> adapterDate= new ArrayAdapter<String>(this,android.
+            R.layout.simple_spinner_dropdown_item ,d);
+
+    date.setAdapter(adapterDate);
+
+    date.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view,
+        int position, long id) {
+            // Get select item
+            int sid=date.getSelectedItemPosition();
+            Toast.makeText(getBaseContext(), "You have selected City : " + d[sid],
+                    Toast.LENGTH_SHORT).show();
+        }
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+            // TODO Auto-generated method stub
+        }
+    });
+
+state =(Spinner)findViewById(R.id.state);
+
+        ArrayAdapter<String> adapterState= new ArrayAdapter<String>(this,android.
+        R.layout.simple_spinner_dropdown_item ,s);
+
+        state.setAdapter(adapterState);
+
+        state.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+@Override
+public void onItemSelected(AdapterView<?> parent, View view,
+        int position, long id) {
+        // Get select item
+        int sid=state.getSelectedItemPosition();
+        Toast.makeText(getBaseContext(), "You have selected City : " + s[sid],
+        Toast.LENGTH_SHORT).show();
+        }
+@Override
+public void onNothingSelected(AdapterView<?> parent) {
+        // TODO Auto-generated method stub
+        }
+        });
+
 
         show.setOnClickListener(new View.OnClickListener() {
             @Override
