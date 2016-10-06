e6cd24732aedfa04d7b5c1a0a950c6f10920d7a6
tushar <tushardma167@gmail.com>
2016-10-06 17:57:05 +0530

    research team & gallery

--- app/src/main/java/com/example/ashish/railtellapp/fragments/ResearchTeam.java
+++ app/src/main/java/com/example/ashish/railtellapp/fragments/ResearchTeam.java
@@ -1,6 +1,7 @@
 package com.example.ashish.railtellapp.fragments;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -8,12 +9,73 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.ashish.railtellapp.R;
+import com.example.ashish.railtellapp.ankush;
+import com.example.ashish.railtellapp.deepak;
+import com.example.ashish.railtellapp.prof;
+import com.example.ashish.railtellapp.shruti;
+import com.example.ashish.railtellapp.tasneem;
 
 
 public class ResearchTeam extends Fragment {
 
+    public Button d1;
+    public void Dharmendra(View view){
+        d1=(Button) view.findViewById(R.id.d1);
+        d1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent detail = new Intent(getActivity(), prof.class);
+                startActivity(detail);
+            }
+        });
+    }
+    public Button d2;
+    public void Scholar1(View view){
+        d2=(Button) view.findViewById(R.id.d2);
+        d2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent detail2=new Intent(getActivity(), tasneem.class);
+                startActivity(detail2);
+            }
+        });
+    }
+    public Button d3;
+    public void Scholar2(View view){
+        d3=(Button) view.findViewById(R.id.d3);
+        d3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent detail3=new Intent(getActivity(), shruti.class);
+                startActivity(detail3);
+            }
+        });
+    }
+    public Button d4;
+    public void Scholar3(View view){
+        d4=(Button) view.findViewById(R.id.d4);
+        d4.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent detail4=new Intent(getActivity(), ankush.class);
+                startActivity(detail4);
+            }
+        }));
+    }
+    public Button d5;
+    public void Scholar4(View view){
+        d5=(Button) view.findViewById(R.id.d5);
+        d5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent detail5=new Intent(getActivity(), deepak.class);
+                startActivity(detail5);
+            }
+        });
+    }
 
 
     @Override
@@ -32,5 +94,10 @@ public class ResearchTeam extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        Dharmendra(view);
+        Scholar1(view);
+        Scholar2(view);
+        Scholar3(view);
+        Scholar4(view);
     }
 }
---
+++ app/src/main/java/com/example/ashish/railtellapp/tasneem.java
@@ -0,0 +1,13 @@
+package com.example.ashish.railtellapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class tasneem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tasneem);
+    }
+}
