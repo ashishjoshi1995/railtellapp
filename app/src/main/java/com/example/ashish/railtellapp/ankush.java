b1ede1bfd4e8faf0abb2d4bcf21443cd8510280e
gaurav <gauravwadhwani.gw@gmail.com>
2016-10-17 12:55:56 +0530

    yo

--- app/src/main/java/com/example/ashish/railtellapp/ankush.java
+++ app/src/main/java/com/example/ashish/railtellapp/ankush.java
@@ -10,14 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link Ankush.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link Ankush#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class Ankush extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
