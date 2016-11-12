45bfb9b39cfd01132ab6331163819a07d5b1f6d0
Ashish <ashish1995joshi@gmail.com>
2016-11-12 16:42:37 +0530

    sunc commit

--- app/src/main/java/com/example/ashish/railtellapp/displayActivities/CropMonitoringDistFirstDisplay.java
+++ app/src/main/java/com/example/ashish/railtellapp/displayActivities/CropMonitoringDistFirstDisplay.java
@@ -2,9 +2,15 @@ package com.example.ashish.railtellapp.displayActivities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.example.ashish.railtellapp.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.squareup.picasso.Picasso;
 
 /**
@@ -12,7 +18,11 @@ import com.squareup.picasso.Picasso;
  */
 
 public class CropMonitoringDistFirstDisplay extends AppCompatActivity {
+
+   // Button show;
     ImageView view1,view2,view3;
+    private GoogleMap mgoogleMap;
+    static final LatLng TutorialsPoint = new LatLng(21 , 57);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,5 +59,25 @@ public class CropMonitoringDistFirstDisplay extends AppCompatActivity {
                 //.error(R.drawable.pic1a)      // optional
                 //.resize(400,400)                        // optional
                 .into(view3);
+/*
+        try {
+            LatLng sydney = new LatLng(Double.parseDouble(ar[1]), Double.parseDouble(ar[2]));
+            mgoogleMap.addMarker(new MarkerOptions().position(sydney));
+            mgoogleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            CameraPosition position = CameraPosition.builder()
+                    .target(sydney)
+                    .zoom(12f)
+                    .bearing(0.0f)
+                    .tilt(0.0f)
+                    .build();
+            mgoogleMap.animateCamera(CameraUpdateFactory
+                    .newCameraPosition(position), null);
+            mgoogleMap.getUiSettings().setZoomControlsEnabled(true);
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        */
     }
 }
